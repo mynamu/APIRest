ca3fbb48ce2f022433ab0e0bee7f0afcf60c4778
mynamu <joycelyderi@gmail.com>
2026-09-13 12:29:25 -0300

    first commit

---
+++ src/main/java/br/com/cantina/Cantina/database/DTO/ProdutoResponseDTO.java
@@ -0,0 +1,63 @@
+package br.com.cantina.Cantina.database.DTO;
+
+import br.com.cantina.Cantina.database.enums.CategoriaProduto;
+import br.com.cantina.Cantina.database.model.Produto;
+
+import java.math.BigDecimal;
+
+public class ProdutoResponseDTO {
+        private final Long id;
+        private final String nome;
+        private final String descricao;
+        private final BigDecimal preco;
+        private final Integer tempoPreparoMinutos;
+        private final Integer quantidadeDisponivelHoje;
+        private final CategoriaProduto categoriaProduto;
+        private final boolean ativo;
+
+        public ProdutoResponseDTO(Produto produto) {
+            if (produto == null) {
+                throw new IllegalArgumentException("Produto não pode ser nulo");
+            }
+            this.id = produto.getId();
+            this.nome = produto.getNome();
+            this.descricao = produto.getDescricao();
+            this.preco = produto.getPreco();
+            this.tempoPreparoMinutos = produto.getTempoPreparoMinutos();
+            this.quantidadeDisponivelHoje = produto.getQuantidadeDisponivelHoje();
+            this.categoriaProduto = produto.getCategoriaProduto();
+            this.ativo = produto.isAtivo();
+        }
+
+    public Integer getTempoPreparoMinutos() {
+        return tempoPreparoMinutos;
+    }
+
+    public CategoriaProduto getCategoriaProduto() {
+        return categoriaProduto;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public Integer getQuantidadeDisponivelHoje() {
+        return quantidadeDisponivelHoje;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
